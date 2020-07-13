80048801e373df13ceb83d34134d0fad8f3a48c2
wujianyong <1694163757@qq.com>
2020-07-13 13:08:47 +0800

    '适配框架'

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <span>中国之星</span>
+    <div class="one">1</div>
+    <div class="two">2</div>
+    <div class="three">3</div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+  span{
+    font-size: 32px;
+  }
+  .one{
+    width:750px;
+    height:50px;
+    background: #42b983;
+  }
+  .two{
+    width:375px;
+    height:50px;
+    background: #000;
+  }
+  .three{
+    width:187px;
+    height:50px;
+    background:yellowgreen;
+  }
+  html,body{
+    padding: 0;
+    margin: 0;
+  }
+</style>
